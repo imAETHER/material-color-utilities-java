5816d32823da3243f4a412404a43f16cd0be5dc2
Ronen <ronenfes@gmail.com>
2025-04-27 01:00:38 +0700

    cleaned

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,19 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
-
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])  // Using the default Java component
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")  // JitPack repository URL
+        }
+    }
+}
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation(libs.annotation.jvm)
